d8d11c2a7edb505a5be4b19ea9ced189ace89c75
IvoGeorgievx <102153817+IvoGeorgievx@users.noreply.github.com>
2025-06-07 19:15:14 +0300

    feat:added budget period

--- src/app/features/components/dashboard/categories/create-category/create-category.component.ts
+++ src/app/features/components/dashboard/categories/create-category/create-category.component.ts
@@ -6,12 +6,15 @@ import {
   Validators,
 } from '@angular/forms';
 import { CategoryStore } from '@core/store/category.store';
+import { CategoryBudgetPeriod } from '@shared/types/category.type';
 import { checkInvalidFields } from '@shared/utils/form-utils';
+import { tap } from 'rxjs';
 
 interface ICategoryForm {
   name: FormControl<string>;
   type: FormControl<string>;
   budget: FormControl<number | null>;
+  budgetPeriod: FormControl<CategoryBudgetPeriod | null>;
 }
 
 @Component({
@@ -23,6 +26,8 @@ interface ICategoryForm {
 export class CreateCategoryComponent implements OnInit {
   categoryStore = inject(CategoryStore);
 
+  budgetPeriod = CategoryBudgetPeriod;
+
   prefix = input.required<string>();
   //prefix is needed cuz the form is used in multiple components with identical identifiers in the DOM which can cause browser issues
 
@@ -31,6 +36,17 @@ export class CreateCategoryComponent implements OnInit {
 
   ngOnInit(): void {
     this.initForm();
+
+    this.categoryForm.get('budget')?.valueChanges.subscribe((value) => {
+      const budgetPeriodControl = this.categoryForm.get('budgetPeriod');
+      if (value !== null) {
+        budgetPeriodControl?.enable();
+        budgetPeriodControl?.addValidators(Validators.required);
+        budgetPeriodControl?.updateValueAndValidity();
+      } else {
+        budgetPeriodControl?.disable();
+      }
+    });
   }
 
   private initForm(): void {
@@ -44,6 +60,10 @@ export class CreateCategoryComponent implements OnInit {
         validators: Validators.required,
       }),
       budget: new FormControl<number | null>(null),
+      budgetPeriod: new FormControl<CategoryBudgetPeriod | null>({
+        value: null,
+        disabled: true,
+      }),
     });
   }
 
@@ -64,5 +84,11 @@ export class CreateCategoryComponent implements OnInit {
 
   checkInvalidField(field: string) {
     return checkInvalidFields(this.categoryForm, field);
+  }
+
+  handleBudgetPeriod(event: Event): void {
+    const value = (event.target as HTMLSelectElement)
+      .value as CategoryBudgetPeriod;
+    this.categoryForm.get('budgetPeriod')?.setValue(value);
   }
 }
--- src/app/shared/types/category.type.ts
+++ src/app/shared/types/category.type.ts
@@ -1,7 +1,14 @@
+export enum CategoryBudgetPeriod {
+  DAILY = 'Daily',
+  WEEKLY = 'Weekly',
+  MONTHLY = 'Monthly',
+}
+
 export interface Category {
   id: string;
   name: string;
   type: string;
   budget: string;
+  budgetPeriod: CategoryBudgetPeriod;
   default: boolean;
 }
